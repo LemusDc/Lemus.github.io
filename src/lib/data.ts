ba11d0b07e3ecfaddb99814564273a959c833de8
LemusDc <xxlemusxx@Gmail.com>
2026-02-15 21:49:02 -0500

    update

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -6,90 +6,58 @@ export const personalInfo = {
   linkedin: "https://co.linkedin.com/",
   profilePicture: "/profile.jpeg",
   heroDescription:
-    "👨‍💻 Estudiante de Ingeniería en la Universidad El Bosque con 6 años de experiencia en seguridad informática y conocimientos en desarrollo web. Apasionado por la tecnología, la ciberseguridad y la creación de soluciones digitales seguras.",
+    "👨‍💻Engineering student at El Bosque University with 6 years of experience in cybersecurity and web development. Passionate about technology, cybersecurity, and creating secure digital solutions.",
 };
 
 export const workExperience = [
   {
-    company: "Lorem Ipsum Corp",
-    location: "Lorem City",
-    position: "Lorem Engineer",
-    period: "Jan 2020 - Present",
+    company: "Teleperformance",
+    location: "Bogota - Colombia",
+    position: "Security Analyst",
+    period: "Oct 2020 - Present",
     achievements: [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      "Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
-      "Duis aute irure dolor in reprehenderit in voluptate velit esse.",
-      "Excepteur sint occaecat cupidatat non proident.",
-      "Sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      "Curabitur pretium tincidunt lacus. Nulla gravida orci a odio.",
-    ],
-  },
-  {
-    company: "Dolor Sit Amet",
-    location: "Ipsumville",
-    position: "Software Developer",
-    period: "Feb 2019 - Jan 2020",
-    achievements: [
-      "Morbi in sem quis dui placerat ornare.",
-      "Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam.",
-    ],
-  },
-  {
-    company: "Consectetur Inc.",
-    location: "Adipiscing, Ipsum",
-    position: "Intern",
-    period: "Jun 2018 - Feb 2019",
-    achievements: [
-      "Praesent dapibus, neque id cursus faucibus.",
-      "Fusce feugiat malesuada odio.",
+      "Extended Knowledge about security breaches.",
+      "Knowledge in Regex.",
     ],
   },
 ];
 
 export const education = [
   {
-    institution: "Lorem Ipsum University",
-    location: "Lorem City",
-    degree: "BSc Lorem Ipsum",
-    period: "2015 - 2019",
+    institution: "Universidad el Bosque",
+    location: "Bogota - Colombia",
+    degree: "Software Enginering",
+    period: "2023 - Present",
     achievements: [
-      "President of Lorem Ipsum Club",
-      "Organized multiple lorem events",
-      "Represented university in national competitions.",
-      "Graduated with honors.",
+      "App Development with Java",
+      "Web Development with Java and Springboot",
     ],
   },
 ];
 export const skills = {
   programmingLanguages: [
-    "LoremLang",
-    "IpsumScript",
-    "DolorLang",
-    "SitLang",
-    "AmetLang",
-    "ConsecteturLang",
+    "C++",
+    "JAVA",
+    "Python",
   ],
   frontendDevelopment: [
-    "LoremJS",
-    "IpsumJS",
-    "Dolor Native",
-    "Sit UI",
-    "Amet CSS",
     "HTML",
     "CSS",
+    "JavaScript",
+    "JSF",
   ],
-  backendDevelopment: ["LoremNode", "IpsumExpress"],
-  databaseAndStorage: ["LoremDB", "IpsumORM"],
-  cloudAndDevOps: ["LoremCloud"],
+  backendDevelopment: [
+    "JAVA",
+     "Python",
+    ],
+  databaseAndStorage: [
+    "Oracle",
+    "Azure"
+  ],
   toolsAndServices: [
-    "LoremAuth",
-    "IpsumCMS",
-    "DolorAnalytics",
-    "SitValidator",
-    "AmetMonitor",
-    "ConsecteturPanel",
-    "AdipiscingTrigger",
+    "Eclipse",
+    "Netbeans",
+    "Intellij",
   ],
 };
 
@@ -118,54 +86,12 @@ export const projects = [
   },
 ];
 
-export const awards = [
+export const Evidences = [
   {
     name: "Lorem Ipsum Award",
     issuer: "Lorem Organization",
     date: "Jan 2020",
     type: "International",
     position: "First Place",
-  },
-  {
-    name: "Dolor Sit Amet Prize",
-    issuer: "Ipsum Foundation",
-    date: "Feb 2021",
-    type: "National",
-    position: "Runner-up",
-  },
-  {
-    name: "Consectetur Hackathon",
-    issuer: "Adipiscing Org",
-    date: "Mar 2022",
-    type: "National",
-    position: "Winner",
-  },
-  {
-    name: "Vestibulum Event",
-    issuer: "Vestibulum College",
-    date: "Apr 2022",
-    type: "National",
-    position: "First Prize",
-  },
-  {
-    name: "Curabitur Hackfest",
-    issuer: "Curabitur Institute",
-    date: "May 2022",
-    type: "National",
-    position: "Second Prize",
-  },
-  {
-    name: "Praesent Hacks",
-    issuer: "Praesent Group",
-    date: "Jun 2022",
-    type: "National",
-    position: "Second Runner-up",
-  },
-  {
-    name: "Fusce Hack",
-    issuer: "Fusce Club",
-    date: "Jul 2022",
-    type: "National",
-    position: "Most Impactful Hack",
   },
 ];
